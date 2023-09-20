91c9f0aa6d5978abd6c4ceaaa4d236f60c82fbe4
Sushil Kumar Mahto <74419540+Sushil-mahto@users.noreply.github.com>
2023-09-20 16:01:22 +0530

    Initial Commit

---
+++ Stayhook/StayhookApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayhookApp.swift
+//  Stayhook
+//
+//  Created by Sushil Mahto on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct StayhookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
